f17acfc0728e1674b66c4927beac3713a2bb486e
mdming <53088374+mdming@users.noreply.github.com>
2019-08-04 00:20:56 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,38 +13,46 @@ document.querySelector('.menu').addEventListener("click", function (e) {
 		document.querySelector('.container-nav').setAttribute('style', 'background-color: #F0F8FF;')
 	}
 })
+// 顶导航栏及置顶键的响应
 window.onload = function () {
-	document.onmousewheel = function () {
-		var e = e || window.event;
-		var a = e.wheelDelta;
-		var st = document.documentElement.scrollTop;
-		if (a < 0) {
+	var st = 1;
+	document.onscroll = function () {
+		if (st < document.documentElement.scrollTop) { //判断页面滚动的方向
 			document.querySelectorAll('.container-nav')[0].style.top = -50 + "px";
 			document.querySelectorAll('.container-nav')[1].style.top = -50 + "px";
+			document.querySelector('.top').style.right = -39 + "px"
 		} else {
 			document.querySelectorAll('.container-nav')[0].style.top = 0 + "px";
 			document.querySelectorAll('.container-nav')[1].style.top = 0 + "px";
-		}
-		if (st > 300) {
 			document.querySelector('.top').style.right = 5 + "px"
-		} else {
-			document.querySelector('.top').style.right = -39 + "px"
 		}
+		if (st < 150) {
+			document.querySelector('.top').style.right = -39 + "px"
+
+		}
+		st = document.documentElement.scrollTop;
 	}
+
+	document.querySelector('label.switch').addEventListener("click", function () {
+		document.querySelector('.nav-m').style.display = "none";
+		document.querySelector('.nav-bottom').style.display = "block";
+		document.querySelector('main').style.padding = "0";
+	})
 }
-var ClientY = 0;
-document.addEventListener("touchstart", function (e) {
-	ClientY = e.changedTouches[0].clientY;
-})
-document.addEventListener("touchend", function (e) {
-	if (ClientY > e.changedTouches[0].clientY) {
-		document.querySelectorAll('.container-nav')[0].style.top = -50 + "px";
-	} else {
-		document.querySelectorAll('.container-nav')[0].style.top = 0 + "px";
-	}
-	if (ClientY > e.changedTouches[0].clientY || document.documentElement.scrollTop < 200) {
-		document.querySelector('.top').style.right = -39 + "px"
-	} else {
-		document.querySelector('.top').style.right = 5 + "px"
-	}
-})
+// var ClientY = 0;
+// document.addEventListener("touchstart", function (e) {
+// 		console.log(22222);
+// 		ClientY = e.changedTouches[0].clientY;
+// })
+// document.addEventListener("touchend", function (e) {
+// 	if (ClientY > e.changedTouches[0].clientY) {
+// 		document.querySelectorAll('.container-nav')[0].style.top = -50 + "px";
+// 	} else {
+// 		document.querySelectorAll('.container-nav')[0].style.top = 0 + "px";
+// 	}
+// 	if (ClientY > e.changedTouches[0].clientY || document.documentElement.scrollTop < 200) {
+// 		document.querySelector('.top').style.right = -39 + "px"
+// 	} else {
+// 		document.querySelector('.top').style.right = 5 + "px"
+// 	}
+// })
